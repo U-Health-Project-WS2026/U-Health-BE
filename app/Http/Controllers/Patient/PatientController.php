0fd9249b402c0de154b2dde0adc73be805d208cc
derhaideer2 <muhammedemir.akguel@gmail.com>
2026-02-03 23:06:40 +0100

    Implementing new ROute for users, to geet personal info

---
+++ app/Http/Controllers/Patient/PatientController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Patient;
+
+use App\Http\Resources\PatientResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Patient;
+class PatientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function show_my_info(Request $request)
+    {
+        //token based searching for user and than patient
+        $user = $request->user();
+        return new PatientResource(Patient::findOrFail($user->user_id));
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api/patient.php
+++ routes/api/patient.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Patient\PatientController;
 
 Route::middleware('auth:sanctum') //User must be auth
 ->prefix('patients') //api request begins with patients/...
 ->group(function(){
 
-    //default
-    Route::get('users', function(){ return "PATIENT";});
+    //personal Infos
+    Route::get('me', [PatientController::class, 'show_my_info']);
 
+    
 });
 
