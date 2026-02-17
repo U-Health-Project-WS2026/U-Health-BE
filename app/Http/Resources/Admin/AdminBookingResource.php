1d8e67e4e04d2385fac63ba94de27fae33e8b461
Paul-SE1 <paul.lopatin@student.uni-tuebingen.de>
2026-02-17 19:09:05 +0100

    Improve AdminBookingResource: embed patient info

--- app/Http/Resources/Admin/AdminBookingResource.php
+++ app/Http/Resources/Admin/AdminBookingResource.php
@@ -6,14 +6,27 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class AdminBookingResource extends JsonResource
 {
-    public function toArray($request): array
-    {
-        return [
-            'booking_id'      => $this->booking_id,
-            'patient_id'      => $this->patient_id,
-            'time_slot_start' => $this->time_slot_start,
-            'time_slot_end'   => $this->time_slot_end,
-            'status'          => $this->status,
-        ];
-    }
+   public function toArray($request)
+{
+    $patient = $this->patients;
+
+    return [
+        'booking_id'      => $this->booking_id,
+
+        'patient' => $patient ? [
+            'patient_id' => $patient->patient_id,
+            'first_name' => $patient->first_name,
+            'last_name'  => $patient->last_name,
+            'name'       => trim(($patient->first_name ?? '').' '.($patient->last_name ?? '')),
+            'age'        => $patient->age,
+            'sex'        => $patient->sex,
+            'location'   => $patient->location,
+        ] : null,
+
+        'time_slot_start' => $this->time_slot_start,
+        'time_slot_end'   => $this->time_slot_end,
+        'status'          => $this->status,
+    ];
+}
+
 }
