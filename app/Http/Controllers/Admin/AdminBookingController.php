887c7ba25869451bad9f1445d528a4fc098df64b
Percy <sebastian@kohrell.de>
2026-02-18 17:35:43 +0100

    fixed some issues

--- app/Http/Controllers/Admin/AdminBookingController.php
+++ app/Http/Controllers/Admin/AdminBookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\AdminBookingResource;
 use App\Models\Booking;
+use Illuminate\Http\Request;
 
 class AdminBookingController extends Controller
 {
