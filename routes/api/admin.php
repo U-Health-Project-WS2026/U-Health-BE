e99bf6b4f8301fb58abcda669a1a193178dae610
Paul-SE1 <paul.lopatin@student.uni-tuebingen.de>
2026-02-22 18:44:08 +0100

    Add Medicine Managment  CRUD + search

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminPatientController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\AdminBookingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminMedicationController;
 
 Route::middleware(['auth:sanctum']) //User must be auth and admin
 ->prefix('admin') //api request begins with admin/...
@@ -17,4 +18,13 @@ Route::middleware(['auth:sanctum']) //User must be auth and admin
     Route::get('bookings', [AdminBookingController::class, 'index']);
     Route::get('bookings/patient/{patientId}', [AdminBookingController::class, 'byPatient']);
     Route::delete('bookings/{bookingId}', [AdminBookingController::class, 'destroy']);
+
+    Route::get('medications', [AdminMedicationController::class, 'index']);
+    Route::post('medications', [AdminMedicationController::class, 'store']);
+    Route::get('medications/{id}', [AdminMedicationController::class, 'show']);
+    Route::match(['put', 'patch'], 'medications/{id}', [AdminMedicationController::class, 'update']);
+    Route::delete('medications/{id}', [AdminMedicationController::class, 'destroy']);
+
+
+
 });
