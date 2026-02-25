bb969bbb006832c507210302ad2972cbd7115029
Percy <sebastian@kohrell.de>
2026-02-25 16:15:16 +0100

    added function viewTreatmentHistory

--- app/Http/Controllers/Patient/PatientTreatmentController.php
+++ app/Http/Controllers/Patient/PatientTreatmentController.php
@@ -11,17 +11,38 @@ class PatientTreatmentController extends Controller
 {
 
     /**
+     * Patient: view entire treatment history
+     * GET /api/v1/patients/treatments/history
+     */
+    public function viewTreatmentHistory(Request $request)
+    {
+        //get current user
+        $user = $request->user();
+
+        //get treatments of patient with the user_id, ordered by date_of_treatment
+        $treatments = $user->patients
+            ->treatments()
+            ->orderBy('date_of_treatment', 'desc')
+            ->get();
+
+        return TreatmentResource::collection($treatments);
+    }
+
+    /**
      * Patient: search treatment by date
-     * GET /api/v1/patients/treatments/date
+     * GET /api/v1/patients/treatments/date?date_of_treatment=yyyy-mm-dd
      */
     public function searchTreatmentDate(Request $request){
 
+        //validate data type
         $request->validate([
-            'date_of_treatment' => 'required|date_of_treatment'
+            'date_of_treatment' => 'required|date'
         ]);
 
+        //get date from the url
         $date = $request->query('date_of_treatment');
 
+        //searches for date_of_treatment(only day), ordered by date_of_treatment
         $treatments = Treatment::whereDate('date_of_treatment', $date)
             ->orderBy('date_of_treatment', 'asc')
             ->get();
--- routes/api/patient.php
+++ routes/api/patient.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Patient\PatientController;
 use App\Http\Controllers\Patient\PatientBookingController;
+use App\Http\Controllers\Patient\PatientTreatmentController;
 
 
 Route::middleware('auth:sanctum') //User must be auth
@@ -14,7 +15,13 @@ Route::middleware('auth:sanctum') //User must be auth
     Route::get('bookings', [PatientBookingController::class, 'viewBookedTimeSlots']);
 
     //search treatment by date
-    Route::get('treatments/date', [\App\Http\Controllers\Patient\PatientTreatmentController::class, 'searchTreatmentDate']);
+    Route::get('treatments/date', [PatientTreatmentController::class, 'searchTreatmentDate']);
+
+    //show entire patient history
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('treatments/history', [PatientTreatmentController::class, 'viewTreatmentHistory']);
+    });
 
     //personal Infos
     Route::get('me', [PatientController::class, 'show_my_info']);
